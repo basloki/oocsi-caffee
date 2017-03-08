18d5c6ab2a42d5bc79e3fed560aa07f0db10ed72
Bas <bas@schleijpen.nl>
2017-03-08 14:32:35 +0100

    Update Coffee.ino

--- Arduino src/Coffee.ino
+++ Arduino src/Coffee.ino
@@ -1,28 +1,40 @@
 #include <ESP8266WiFi.h>
 #include "OOCSI.h"
+#include <Servo.h> 
 
 const char* OOCSIName = "your_name";    //name for connecting with OOCSI
-const char* hostserver = "oocsi.id.tue.nl";     //put the adress of your OOCSI server here, can be a web page or an IP adres string
-const char* ssid = "ssid_name";                    //SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
-const char* password = "supersecretpassword";            //Password of your Wifi network.
-const char* OOCSIChannel = "coffee_channel";       //name of the channel you want to subscribe to (can subscribe to multiple channels)
+const char* hostserver = "oocsi.id.tue.nl";  //adress of OOCSI server
+const char* ssid = "wifiname"; //SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
+const char* password = "wifipass"; //Password of your Wifi network.
+const char* OOCSIChannel = "coffee_channel"; //name of the channel
+
+Servo myservo;
 
 void proccessOOCSI();
 
 OOCSI oocsi(OOCSIName, hostserver, ssid, password, proccessOOCSI);  //setting up OOCSI. processOOCSI is the name of the fucntion to call when receiving messages, can be a random function name.
 
-const int c_queueSize = 10;
+// declare caffee variables
+const int c_queueSize = 9001; // max number of orders that can be in the queue, currently it is just over 9000
+const int c_machineTime = 5; // minutes the machine takes to prepare a cup of coffee
+const int c_bundleOrdersTime = 30 // minutes that other coffee orders should be bundled
+
+// coffee system arrays
 int coffeeLine[c_queueSize][3]; // [int userID, int amountOfCoffee, int whenToMake]
+int prepareLine[c_queueSize][3]; // [int userID, int amountOfCoffee]
 
 void setup() {
-
+//*************************  
+  myservo.attach(0);
+//*************************
+  
   for(int i = 0; i < c_queueSize; i = i + 1){
       coffeeLine[i][0] = 0;
       coffeeLine[i][1] = 0;
       coffeeLine[i][2] = 0;
     }
   
-  // put your setup code here, to run once:
+  // setup the serial
   Serial.begin(115200);
   Serial.println("connecting Wifi");
   oocsi.connectOocsi();           //connect wifi and OOCSI to the server.
@@ -43,12 +55,24 @@ void setup() {
 }
 
 void loop() {
+//*************************
+  int pos;
+
+  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+  } 
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+  }
+//*************************
   // put your main code here, to run repeatedly:
   oocsi.check();  //function needs to be checked in order for OOCSI to process incoming data.
-
+  
   // check if coffee needs to be made
   for(int i = 0; i < c_queueSize; i = i + 1){
-    if(coffeeLine[i][0] > 0 && coffeeLine[i][2] < millis()/60000){ // find orders
+    if(coffeeLine[i][0] > 0 && coffeeLine[i][2] < ((millis()/60000)-c_machineTime)){ // find orders
       // make the coffee
       iWantCoffeeNow(coffeeLine[i][1], coffeeLine[i][0]);
       // clear the line in the queue
@@ -57,6 +81,19 @@ void loop() {
       coffeeLine[i][2] = 0;
     }
   }
+  
+  // check for coffee that is ready
+  for(int i = 0; i < c_queueSize; i = i + 1){
+    if(prepareLine[i][0] > 0 && prepareLine[i][2] < (millis()/60000)){ // find orders
+      // say to oocsi that the coffee is ready
+      coffeeoutput(4, prepareLine[i][1], prepareLine[i][0], "COFFEE IS READY"); 
+      // clear the line in the queue
+      prepareLine[i][0] = 0;
+      prepareLine[i][1] = 0;
+      prepareLine[i][2] = 0;
+    }
+  }
+  
   
 }
 
@@ -85,7 +122,6 @@ void proccessOOCSI(){
     scheduleCoffee(caffee_who, caffee_amount, caffee_time_to_wait);
   }
 
-  //displaySomeOOCSIData();
 }
 
 void scheduleCoffee(int who, int howmuch, int when){
@@ -113,29 +149,52 @@ void scheduleCoffee(int who, int howmuch, int when){
 }
 
 void iWantCoffeeNow(int howMuchCoffee, int who){
+  
+  int timeItWillBeReady = (millis()/60000)+c_machineTime;
+  
+  // say the order started to oocsi
+  coffeeoutput(3, howMuchCoffee, who, "COFFEE IS BEING PREPARED");
+  // add it to the preparation queue
+  for(int i = 0; i < c_queueSize; i = i + 1){
+    if(prepareLine[i][0] == 0){ // find an empty line in the queue
+      prepareLine[i][0] = who;
+      prepareLine[i][1] = howMuchCoffee;
+      prepareLine[i][2] = timeItWillBeReady;
+      break;
+    }
+  }
+
+  // see if there are other orders for coffee within the next c_bundleOrdersTime minutes
+  for(int i = 0; i < c_queueSize; i = i + 1){
+    if(coffeeLine[i][0] > 0 && coffeeLine[i][2] < ((millis()/60000)-c_bundleOrdersTime)){ // find orders
+      // add to the amount
+      howMuchCoffee = howMuchCoffee + coffeeLine[i][1];
+      // say the order started to oocsi
+      coffeeoutput(3, coffeeLine[i][1], coffeeLine[i][0], "COFFEE IS BEING PREPARED");
+      // add to the preparation queue
+      for(int i = 0; i < c_queueSize; i = i + 1){
+        if(prepareLine[i][0] == 0){ // find an empty line in the queue
+          prepareLine[i][0] = who;
+          prepareLine[i][1] = howmuch;
+          prepareLine[i][2] = timeItWillBeReady;
+          break;
+        }
+      }
+      // clear the line in the order queue
+      coffeeLine[i][0] = 0;
+      coffeeLine[i][1] = 0;
+      coffeeLine[i][2] = 0;
+    }
+  }
+  
+  // prepare the coffee
   Serial.println("CAFFEEEE: OK I WILL MAKE COFFEE!");
   Serial.print("CAFFEEEE: ");
   Serial.print(howMuchCoffee);
   Serial.print(" CUPS");
-  // output someting to oocsi
-  coffeeoutput(3, howMuchCoffee, who, "COFFEE IS BEING PREPARED");
-  // output someting to oocsi
-  delay(1000); // for now the coffee is ready immediately
-  coffeeoutput(4, howMuchCoffee, who, "COFFEE IS READY"); 
-}
-
-void displaySomeOOCSIData(){
-  Serial.println("\t sender: ");
-  Serial.println(oocsi.getSender());
-  Serial.println("\t recipient: ");
-  Serial.println(oocsi.getRecipient());
-  Serial.println("\t sender: ");
-  Serial.println(oocsi.getSender()); 
-  Serial.println("\t Timestamp: ");
-  Serial.println(oocsi.getTimeStamp());
-  Serial.println();
 
 }
+
 void coffeeoutput(int output_type, int amount, int who, String message) {
   oocsi.newMessage((String) OOCSIChannel);  //
   oocsi.sendInt("output_type", output_type);
